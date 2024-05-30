225b1a40b7e3a4091df1c9b91d78f8d2c9622d40
Saing Choch <saing.choch@princebank.com.kh>
2024-05-30 13:37:40 +0700

    initial commit

---
+++ src/test/java/com/mycompany/learn_jwt/LearnJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.learn_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
